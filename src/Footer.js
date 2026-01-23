bcba8ef062122cbad006009d96e02698180f49b7
AdekunleBamz <bams.kunle@gmail.com>
2026-01-23 19:13:44 +0100

    Add Footer component

---
+++ src/Footer.js
@@ -0,0 +1,7 @@
+export function Footer() {
+  return `
+    <footer style="background-color: #333; color: white; padding: 10px; text-align: center;">
+      <p>&copy; 2024 SNF NFT Mint. All rights reserved.</p>
+    </footer>
+  `;
+}
